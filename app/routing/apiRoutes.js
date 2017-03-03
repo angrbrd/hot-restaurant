cba81a9e5866b4ab3e25ebe86dc71e5430582c59
Anna D Derbakova <hiopa16@gmail.com>
2017-03-02 20:57:50 -0500

    Adding /add route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,19 +12,41 @@ var waitlist = require('../data/waitingList.js');
 // ROUTING
 // ===============================================================================
 
-console.log('tables = ' + JSON.stringify(tables));
-console.log('waitlist = ' + JSON.stringify(waitlist));
+// console.log('tables = ' + JSON.stringify(tables));
+// console.log('waitlist = ' + JSON.stringify(waitlist));
 
+// Define the maximum number of tables available
+var maxTables = 5;
+
+// Define the api routes
 module.exports = function(app) {
+	// Return the currently reserved tables
 	app.get('/api/tables', function(req, res) {
 		console.log('__ENTER /api/tables__');
 
 		res.json(tables);
 	});
 
+	// Return the current waitlist
 	app.get('/api/waitlist', function(req, res) {
-		console.log('__ENTER waitlist__');
+		console.log('__ENTER /api/waitlist__');
 		
 		res.json(waitlist);
+	});
+
+	// Add a new reservation
+	app.post('/add', function(req, res) {
+		console.log('__ENTER /add__');
+
+		// Capture the reservation object
+		var newReservation = req.body;
+
+		if(tables.length < maxTables) {
+			tables.push(newReservation);
+			res.json({status: 'OK', location: 'tables'});
+		} else {
+			waitlist.push(newReservation);
+			res.json({status: 'OK', location: 'waitlist'});
+		}
 	});
 };
